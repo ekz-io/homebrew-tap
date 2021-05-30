10c9a0473eed5366d8956cf84c256ba002065c4f
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-30 15:25:11 +0700

    Brew formula update for ekz version v0.5.2-rc.2

--- Formula/ekz.rb
+++ Formula/ekz.rb
@@ -5,20 +5,20 @@
 class Ekz < Formula
   desc "The command line for EKZ, an EKS-D Kubernetes distro."
   homepage "https://github.com/chanwit/ekz/"
-  version "0.5.2-rc.1"
+  version "0.5.2-rc.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.1/ekz_0.5.2-rc.1_darwin_amd64.tar.gz"
-    sha256 "c36b7ebd0bb9445c696cb99b40ce395029811ce6e44fde16482a96cf2a4d1566"
+    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.2/ekz_0.5.2-rc.2_darwin_amd64.tar.gz"
+    sha256 "f94c07f324973c73395eba41a599545b6497bc74aeddf89489ed1074aa0bd222"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.1/ekz_0.5.2-rc.1_darwin_arm64.tar.gz"
-    sha256 "7821775e4097b5ca9c0a22cf8a94f9304211bc451ba9e2ca27129012ebbdb0ac"
+    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.2/ekz_0.5.2-rc.2_darwin_arm64.tar.gz"
+    sha256 "720c3afe03f470c3b0b692289a7edaa2210036c5f3b4c87d07c337ba676a94b7"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.1/ekz_0.5.2-rc.1_linux_amd64.tar.gz"
-    sha256 "775e812440afbc1c9307116f221622afd4e2e5d5b0b455f6dd6e3850111362cc"
+    url "https://github.com/chanwit/ekz/releases/download/v0.5.2-rc.2/ekz_0.5.2-rc.2_linux_amd64.tar.gz"
+    sha256 "f033d574526d27c49dccc99e6a44b7bf9d49f57bf559fe171bd17967993cce80"
   end
 
   depends_on "kubectl" => :optional
